a040f2f6c26715b53e5474ec2cc322e042a94ea6
Melani Romero <m.romero99@hotmail.com>
2020-10-21 11:41:16 -0300

    Primer Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CargarSaldoComponent } from './pages/cargar-saldo/cargar-saldo.component';
+import { SubesComponent } from './pages/subes/subes.component';
+
+import { SubeInfoComponent } from './pages/sube-info/sube-info.component';
+
+
+const routes: Routes = [
+  { path: '', component: SubesComponent},
+  { path: ':id', component: SubeInfoComponent},
+  {path: ':id/cargarSaldo', component: CargarSaldoComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SaldoComponent } from './pages/saldo/saldo.component';
+import { CargarSaldoComponent } from './pages/cargar-saldo/cargar-saldo.component';
+import { SubesComponent } from './pages/subes/subes.component';
+import { ViajesComponent } from './pages/viajes/viajes.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './shared/header/header.component';
+import { SubeInfoComponent } from './pages/sube-info/sube-info.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SaldoComponent,
+    CargarSaldoComponent,
+    SubesComponent,
+    ViajesComponent,
+    HeaderComponent,
+    SubeInfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
